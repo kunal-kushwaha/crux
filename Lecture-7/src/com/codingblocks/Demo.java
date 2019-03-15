d4d06bd269feecc2ff569cccbe7c9daddb6e2a46
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-03-15 14:38:36 +0530

    files modified
    Signed-off-by: Kunal Kushwaha <kunalkushwaha453@gmail.com>

---
+++ Lecture-7/src/com/codingblocks/Demo.java
@@ -0,0 +1,17 @@
+package com.codingblocks;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        if(true){
+            int b = 30;
+        }
+//        System.out.println(b);  // Error
+
+        {
+            int c = 90;
+        }
+//        System.out.println(c);    // Error
+    }
+}
